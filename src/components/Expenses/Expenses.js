b1682d186428f43e400b77e18ecf6e53cff77fe4
rab0ta <aleksei.silber@tptlive.ee>
2024-04-11 14:56:09 +0300

    added filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,13 @@
 import './Expenses.css'
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
+import ExpensesFilter from './ExpenseFilter'
 
 function Expenses(props){
+    
     return(
         <Card className='expenses'>
+            <ExpensesFilter />
             <ExpenseItem expenseData={props.expenses[0]}/>
             <ExpenseItem expenseData={props.expenses[1]}/>
         </Card>
