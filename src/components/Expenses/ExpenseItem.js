10e1b493b8b41da91421044e88294dc8256ed9f8
rab0ta <aleksei.silber@tptlive.ee>
2024-04-11 13:32:53 +0300

    sündmuste töötlusd komponentides

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,17 @@
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
+import { useState } from 'react'
 
 
 function ExpenseItem(props){
-
+    const [title, setTitle] = useState(props.expenseData.title)
+    const clickHandler = () =>{
+        console.log('click!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+        props.expenseData.price = title
+    }
     return (
     <Card className='expense-item'>
         <ExpenseDate 
@@ -14,6 +21,7 @@ function ExpenseItem(props){
             <h2>{props.expenseData.title}</h2>
             <div className='expense-item__price'>{props.expenseData.price}</div>
         </div>
+        <button onClick={clickHandler}>Click Me</button>
     </Card>
     )
 }
